f63d92aff9671d18dd448da8c9cf765503d318ec
Luiz Filipe Magalhães Freitas <felipe22331@gmail.com>
2026-08-25 15:38:52 -0300

    added: exercise 2.20

--- Part_2/countries/src/App.js
+++ Part_2/countries/src/App.js
@@ -1,27 +1,58 @@
 import './index.css';
 import Filter from './components/Filter';
 import Countrie from './components/Countrie';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [countriesToShow, setCountriesToShow] = useState('');
   const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
   
-  const promise = axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
-    .then(response => {
-      setCountries(response.data);
-    })
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => {
+        setCountries(response.data);
+      })
+  }, [])
+
+  useEffect(() => {
+    if (selectedCountry) {
+      axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${selectedCountry.capitalInfo.latlng[0]}&longitude=${selectedCountry.capitalInfo.latlng[1]}&current=temperature_2m,wind_speed_10m`)
+        .then(response => {
+          setWeather(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching weather data:', error);
+        });
+    }
+  }, [selectedCountry]);
 
   const filteredCountries = () => {
-    return countries.filter(country => country.name.common.toLowerCase().includes(countriesToShow.toLowerCase()))
+    const filtered = countries.filter(country => country.name.common.toLowerCase().includes(countriesToShow.toLowerCase()))
+
+    if (filtered.length === 1) {
+      setSelectedCountry(filtered[0]);
+    }
+    return filtered;
   }
 
 
   return(
     <div>
-      <Filter value={countriesToShow} onChange={event => setCountriesToShow(event.target.value)} />
-      <Countrie country={filteredCountries()} />
+      <Filter 
+        value={countriesToShow}
+        onChange={event => {
+          setCountriesToShow(event.target.value)
+          setSelectedCountry(null)
+        }} />
+      
+      <Countrie
+        country={selectedCountry ? [selectedCountry] : filteredCountries()}
+        onShow={setSelectedCountry}
+        weather={weather}
+      />
     </div>
   )
 }
--- Part_2/countries/src/components/Countrie.js
+++ Part_2/countries/src/components/Countrie.js
@@ -1,7 +1,8 @@
-const Country = ({ country }) => {
+const Country = ({ country, onShow, weather }) => {
     if (country.length === 0) {
         return <p>No countries found</p>
     }
+
     if (country.length > 10) {
         return (
             <div>
@@ -9,27 +10,37 @@ const Country = ({ country }) => {
             </div>
         )
     }
+    
     if (country.length > 1) {
         return (
             <div>
                 {country.map((c) => (
-                    <p key={c.name.common}>{c.name.common}</p>
+                    <p key={c.name.common}>
+                        {c.name.common}
+                        <button onClick={() => onShow(c)}>show</button>
+                    </p>
                 ))}
+
             </div>
         )
     }
+
     return (
         <div>
-        <h2>{country[0].name.common}</h2>
-        <p>capital {country[0].capital}</p>
-        <p>area {country[0].area}</p>
-        <h3>languages:</h3>
-        <ul>
-            {Object.values(country[0].languages).map((language) => (
-                <li key={language}>{language}</li>
-            ))}
-        </ul>
-        <img src={country[0].flags.png} alt={`Flag of ${country[0].name.common}`} />
+            <h2>{country[0].name.common}</h2>
+            <p>capital {country[0].capital}</p>
+            <p>area {country[0].area}</p>
+            <h3>languages:</h3>
+            <ul>
+                {Object.values(country[0].languages).map((language) => (
+                    <li key={language}>{language}</li>
+                ))}
+            </ul>
+            <img src={country[0].flags.png} alt={`Flag of ${country[0].name.common}`} />
+            
+            <h2> Weather in {country[0].capital}</h2>
+            <p>Temperature: {weather ? `${weather.current.temperature_2m} °C` : 'Loading...'}</p>
+            <p>Wind {weather ? `${weather.current.wind_speed_10m} Km/h` : 'Loading...'}</p>
         </div>
 
     )
